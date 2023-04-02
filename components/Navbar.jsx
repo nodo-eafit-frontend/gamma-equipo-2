5f056766ea7725ab18f6d9f9e02e19fd4c3ff2c9
JuanPabloRP <jpabloroldanp@gmail.com>
2023-04-01 21:56:01 -0500

    avance en navbar component

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,7 +1,25 @@
 import React from 'react';
+import Link from 'next/link';
+
+const links = [
+	{
+		label: 'Inicio',
+		route: '/',
+	},
+];
 
 const Navbar = () => {
-	return <div>Navbar</div>;
+	return (
+		<nav>
+			<ul>
+				{links.map(({ label, route }) => (
+					<li key={route}>
+						<Link href={route}>{label}</Link>
+					</li>
+				))}
+			</ul>
+		</nav>
+	);
 };
 
 export default Navbar;
