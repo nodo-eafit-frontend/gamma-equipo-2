00ca24fc98aa19732823180118bfe35d288bd6ad
JohanitaM <jole360@hotmail.es>
2023-04-21 18:11:14 -0500

    Feat: boton consultar certificados

--- app/(navbar-micrositio)/certificados/page.jsx
+++ app/(navbar-micrositio)/certificados/page.jsx
@@ -1,16 +1,22 @@
+'use client'
 
 import Form from "@/components/certificados/Form/Form";
 import Historialdonaciones from "@/components/certificados/HistorialDonaciones/HistorialDonaciones";
 import Titulo from "@/components/certificados/Titulo/Titulo";
 import { ButtonDonar } from "@/components/paginaInicial/Button";
+import { useState } from "react";
+
+
 
 const Certificados = () => {
+
+  const [registro, useRegistro] = useState(false);
   return (
     <div>
       <Titulo />
       <ButtonDonar />
-      <Form />
-      <Historialdonaciones />
+      <Form useRegistro={useRegistro}/>
+      <Historialdonaciones registro={registro}/>
     </div>
   );
 };
--- components/certificados/Form/Form.jsx
+++ components/certificados/Form/Form.jsx
@@ -1,25 +1,33 @@
+// 'use client'
+
+import { useState } from "react";
 import styles from "./Form.module.scss";
 import Link from "next/link";
 
 
-export default function Form () {
+export default function Form ({useRegistro}) {
   const { form, form__group, form__input, form__button } = styles;
 
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    useRegistro(true)
+  }
+
   return (
-    <form className={form}>
+    <form onSubmit={handleSubmit} className={form}>
       <div className={form__group}>
         <label htmlFor="tipo-documento">Tipo de documento</label>
         <select id="tipo-documento" className={form__input}>
           <option disabled selected>
             Seleccione su tipo de documento
           </option>
-          <option>Documento de identidad país de origen</option>
-          <option>Documento naconional identidad</option>
+          <option>Cédula de ciudadanía</option>
+          <option>Cédula de extranjería</option>
           <option>Identificación nacional</option>
           <option>NIT</option>
           <option>Pasaporte</option>
-          <option>Cédula de ciudadanía</option>
-          <option>Cédula de extranjería</option>
+          <option>Documento de identidad país de origen</option>
+          <option>Documento nacional identidad</option>
         </select>
       </div>
       <div className={form__group}>
@@ -27,10 +35,8 @@ export default function Form () {
         <input className={form__input} id="numero-documento" type="text" />
       </div>
       <div className={styles["form__group--buttons"]}>
-        <button>
-          <Link href="donar" className={`${form__button} ${styles["form__button--primary"]}`}>
+        <button className={`${form__button} ${styles["form__button--primary"]}`}>
             Consultar Certificados
-          </Link>
         </button>
         <button>
           <Link href="/" className={`${form__button} ${styles["form__button--secondary"]}`} >
