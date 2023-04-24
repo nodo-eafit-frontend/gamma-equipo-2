8cbc4060c841acf9500f3df6e8a9e37cece9fe8a
JuanPabloRP <jpabloroldanp@gmail.com>
2023-04-24 16:23:23 -0500

    feat: cambio en componente Form para poder desplegar

--- components/certificados/Form/Form.jsx
+++ components/certificados/Form/Form.jsx
@@ -1,49 +1,53 @@
 // 'use client'
 
-import { useState } from "react";
-import styles from "./Form.module.scss";
-import Link from "next/link";
+import { useState } from 'react';
+import styles from './Form.module.scss';
+import Link from 'next/link';
 
+export default function Form({ useRegistro }) {
+	const { form, form__group, form__input, form__button } = styles;
 
-export default function Form ({useRegistro}) {
-  const { form, form__group, form__input, form__button } = styles;
+	const HandleSubmit = (event) => {
+		event.preventDefault();
+		useRegistro(true);
+	};
 
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    useRegistro(true)
-  }
-
-  return (
-    <form onSubmit={handleSubmit} className={form}>
-      <div className={form__group}>
-        <label htmlFor="tipo-documento">Tipo de documento</label>
-        <select id="tipo-documento" className={form__input}>
-          <option disabled selected>
-            Seleccione su tipo de documento
-          </option>
-          <option>Cédula de ciudadanía</option>
-          <option>Cédula de extranjería</option>
-          <option>Identificación nacional</option>
-          <option>NIT</option>
-          <option>Pasaporte</option>
-          <option>Documento de identidad país de origen</option>
-          <option>Documento nacional identidad</option>
-        </select>
-      </div>
-      <div className={form__group}>
-        <label htmlFor="numero-documento">Número de documento</label>
-        <input id="numero-documento" type="text" className={form__input}/>
-      </div>
-      <div className={styles["form__group--buttons"]}>
-        <button className={`${form__button} ${styles["form__button--primary"]}`}>
-            Consultar Certificados
-        </button>
-        <button>
-          <Link href="/" className={`${form__button} ${styles["form__button--secondary"]}`} >
-            Regresar
-          </Link>
-        </button>
-      </div>
-    </form>
-  );
+	return (
+		<form onSubmit={HandleSubmit} className={form}>
+			<div className={form__group}>
+				<label htmlFor="tipo-documento">Tipo de documento</label>
+				<select id="tipo-documento" className={form__input}>
+					<option disabled selected>
+						Seleccione su tipo de documento
+					</option>
+					<option>Cédula de ciudadanía</option>
+					<option>Cédula de extranjería</option>
+					<option>Identificación nacional</option>
+					<option>NIT</option>
+					<option>Pasaporte</option>
+					<option>Documento de identidad país de origen</option>
+					<option>Documento nacional identidad</option>
+				</select>
+			</div>
+			<div className={form__group}>
+				<label htmlFor="numero-documento">Número de documento</label>
+				<input id="numero-documento" type="text" className={form__input} />
+			</div>
+			<div className={styles['form__group--buttons']}>
+				<button
+					className={`${form__button} ${styles['form__button--primary']}`}
+				>
+					Consultar Certificados
+				</button>
+				<button>
+					<Link
+						href="/"
+						className={`${form__button} ${styles['form__button--secondary']}`}
+					>
+						Regresar
+					</Link>
+				</button>
+			</div>
+		</form>
+	);
 }
