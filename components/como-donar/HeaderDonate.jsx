7c59abcf285dfe962238a7887d572d97ac7bd79e
JohanitaM <jole360@hotmail.es>
2023-04-24 12:40:56 -0500

    feat: añadir enlace al boton siguiente de donaciones 1

--- components/como-donar/HeaderDonate.jsx
+++ components/como-donar/HeaderDonate.jsx
@@ -4,7 +4,7 @@ import '../../styles/components/como-donar/comoDonar.scss';
 export default function HeaderDonate () {
   return (
     <header class="header">
-        <h1>Cómo donar</h1>
+        <h1>Cómo donar</h1><br />
         <h2>En filantropía EAFIT contamos con diferentes canales para facilitar el proceso de donación.</h2>
     </header>
   )
