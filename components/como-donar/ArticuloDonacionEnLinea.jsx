544e7c347a67e542d6478484e77ec7d936751888
lvaleriaescobar <valeriamedinaes@gmail.com>
2023-04-24 11:38:57 -0500

    cambio en importar estilos en Como Donaar

--- components/como-donar/ArticuloDonacionEnLinea.jsx
+++ components/como-donar/ArticuloDonacionEnLinea.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import imgDonacionLinea from '../../assets/como-donar/donacionlinea.png';
 import Image from 'next/image';
+import '../../styles/components/como-donar/comoDonar.scss';
 
 export default function ArticuloDonacionEnLinea() {
   return (
