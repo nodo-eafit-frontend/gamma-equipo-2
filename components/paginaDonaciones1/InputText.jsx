506d3a6f7192d44a08a98758667fdfb0384935d4
DJAidennn <samualvarezh@gmail.com>
2023-04-24 16:24:50 -0500

    feat: agregar selects al formulario

--- components/paginaDonaciones1/InputText.jsx
+++ components/paginaDonaciones1/InputText.jsx
@@ -1,10 +1,16 @@
 import React from "react";
-import '../../styles/components/paginaDonaciones1/_input-text.scss';
+import "../../styles/components/paginaDonaciones1/_input-text.scss";
 
 export const InputText = (props) => {
-    return (<div>
-        <label htmlFor={props.id}>{props.label}</label>
-        <input className="inputText" type={props.type} id={props.id} placeholder={props.placeholder} />
+  return (
+    <div>
+      <label htmlFor={props.id}>{props.label}</label>
+      <input
+        className="inputText"
+        type={props.type}
+        id={props.id}
+        placeholder={props.placeholder}
+      />
     </div>
-    )
-}
+  );
+};
