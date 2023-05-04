898057ad3bb57021e1914877d9c8d17f8d69cdea
Juan Pablo Roldán Patiño <56945649+JuanPabloRP@users.noreply.github.com>
2023-05-04 15:39:52 +0000

    feat: arreglo en los estilos de como donar

--- app/(navbar-publico)/como-donar/page.jsx
+++ app/(navbar-publico)/como-donar/page.jsx
@@ -1,43 +1,39 @@
-import ArticuloCheque from "@/components/como-donar/ArticuloCheque";
-import ArticuloConvenioBancolombia from "@/components/como-donar/ArticuloConvenioBancolombia";
-import ArticuloDeduccion from "@/components/como-donar/ArticuloDeduccion";
-import ArticuloDonacionEnLinea from "@/components/como-donar/ArticuloDonacionEnLinea";
-import ArticuloEspecie from "@/components/como-donar/ArticuloEspecie";
-import ArticuloPersonalmente from "@/components/como-donar/ArticuloPersonalmente";
-import ArticuloTiempoTalento from "@/components/como-donar/ArticuloTiempoTalento";
-import ArticuloTransferencia from "@/components/como-donar/ArticuloTransferencia";
-import HeaderDonate from "@/components/como-donar/HeaderDonate";
+import ArticuloCheque from '@/components/como-donar/ArticuloCheque';
+import ArticuloConvenioBancolombia from '@/components/como-donar/ArticuloConvenioBancolombia';
+import ArticuloDeduccion from '@/components/como-donar/ArticuloDeduccion';
+import ArticuloDonacionEnLinea from '@/components/como-donar/ArticuloDonacionEnLinea';
+import ArticuloEspecie from '@/components/como-donar/ArticuloEspecie';
+import ArticuloPersonalmente from '@/components/como-donar/ArticuloPersonalmente';
+import ArticuloTiempoTalento from '@/components/como-donar/ArticuloTiempoTalento';
+import ArticuloTransferencia from '@/components/como-donar/ArticuloTransferencia';
+import HeaderDonate from '@/components/como-donar/HeaderDonate';
 import '../../../styles/components/como-donar/comoDonar.scss';
 
 const DonateContainer = () => {
-    return (
+	return (
+		<div className="comoDonar">
+			<HeaderDonate />
+			<main className="content">
+				<section className="content-como-donar">
+					<ArticuloDonacionEnLinea />
+					<hr />
+					<ArticuloConvenioBancolombia />
+					<hr />
+					<ArticuloTransferencia />
+					<hr />
+					<ArticuloCheque />
+					<hr />
+					<ArticuloPersonalmente />
+					<hr />
+					<ArticuloDeduccion />
+					<hr />
+					<ArticuloTiempoTalento />
+					<hr />
+					<ArticuloEspecie />
+				</section>
+			</main>
+		</div>
+	);
+};
 
-      <div className="page">
-        <HeaderDonate />
-        <main class="content">
-          <section class="content-como-donar">
-
-            <ArticuloDonacionEnLinea />
-            <hr />
-            <ArticuloConvenioBancolombia />
-            <hr />
-            <ArticuloTransferencia />
-            <hr />
-            <ArticuloCheque />
-            <hr />
-            <ArticuloPersonalmente />
-            <hr />
-            <ArticuloDeduccion />
-            <hr />
-            <ArticuloTiempoTalento />
-            <hr />
-            <ArticuloEspecie />
-
-
-          </section>
-        </main>
-      </div>
-    );
-  };
-  
 export default DonateContainer;
